50d17a50e1f441f9e87de444baed19c0be1ce023
leo bang <ryanleosb@gmail.com>
2018-03-13 04:38:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ function logWhisper (string){
   console.log(string.toLowerCase())
 };
 
-function sayHitoGrandma(string){
-  if (string.TolowerCase() === string) {
-    return "I can't hear you!"
-  } else if (string.toUpperCase() === string ) {
-    return "YES INDEED!";
-  } else if (string === "I love you, Grandma"){
-    return "I love you, too";
-  }
-};
+function sayHiToGrandma(string) {
++  if (string.toLowerCase() === string) {
++    return "I can\'t hear you!";
++  } else if (string.toUpperCase() === string) {
++    return "YES INDEED!";
++  } else if (string === "I love you, Grandma.") {
++    return "I love you, too.";
++  }
++};
